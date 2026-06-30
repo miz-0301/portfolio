8d057e73d869072cf1fd9419856f96fabd4513f8
miz-0301 <noguchimizuki0301@gmail.com>
2026-06-30 14:59:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("nav");
 
-menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
+if (menuBtn && nav) {
+    menuBtn.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
